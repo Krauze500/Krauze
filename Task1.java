4f802fd7018b09df608e4fe430c8dbf09c92c751
Krauze500 <64036978+Krauze500@users.noreply.github.com>
2020-04-25 09:20:36 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,16 @@
+package by.belhard.j22.homeworks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int r = 5;{
+
+            if (r>=0){
+            double area = Math.PI * (r * r);
+            System.out.println("Площадь круга равна: " + area);}
+            else{
+            System.out.println("Невалидность введенных данных");}
+            double circumference= Math.PI * 2*r;
+            System.out.println("Длины окружности равна: " + circumference);}}}
+
+
+
