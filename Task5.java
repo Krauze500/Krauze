702eaa2cb0032abb02ce5000fff1dad89fc5cb97
Krauze500 <64036978+Krauze500@users.noreply.github.com>
2020-04-26 11:40:57 +0300

    Add files via upload

---
+++ Task5.java
@@ -0,0 +1,18 @@
+package by.belhard.j22.homeworks;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=100; i++) {
+            if (i%3==0&&i%7==0)
+                System.out.print(i + " ");}
+                System.out.println();
+        for (int i = 0; i <=100; i++){
+            if (i%5==0||i%7==0)
+                System.out.print(i+" ");
+        }
+
+
+
+
+    }}
+
